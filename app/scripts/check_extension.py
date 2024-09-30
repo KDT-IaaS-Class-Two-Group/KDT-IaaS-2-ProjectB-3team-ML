a2cbc2b404c2b51265bd62c38d1854b299ac236d
김정수 <kjs20151240@gmail.com>
2024-09-30 14:35:36 +0900

    :sparkles: 김정수 : 확장자를 확인하여 True,False반환

---
+++ app/scripts/check_extension.py
@@ -0,0 +1,9 @@
+# 확장자를 확인하는 기능
+def allowed_file(filename, allowed_extenstions):
+    """
+    파일 이름이 특정 확장자 중 하나인지 확인합니다.
+
+    :param filename: 검사할 파일 이름
+    :return: 확장자가 허용된 파일일 경우 True, 그렇지 않으면 False
+    """
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extenstions
