66018ed602a7d6968231e9743061f1769a9633a3
이종수 <whdtnxd@gmail.com>
2024-10-07 18:04:18 +0900

    :package: 이종수: 모델 예측, 이미지 전처리 모듈 임포트

--- modules/ensemble/ensemble_predict.py
+++ modules/ensemble/ensemble_predict.py
@@ -1,3 +1,6 @@
+from modules.image.preprocess_image import preprocess_image
+from modules.model.predict_model import predict_model
+
 def ensemble_predict(models, image_path):
     """앙상블 예측 함수"""
     img = preprocess_image(image_path)
